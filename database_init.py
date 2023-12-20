bf15144a2ddd34fd56af3d0cd95cdb2ad1611929
larkJennice <zx2395@columbia.edu>
2023-12-20 08:36:47 +0000

    Adding a delete_database function with modified User table structure

--- database_init.py
+++ database_init.py
@@ -32,8 +32,8 @@ def create_database_and_users_table(db_name, db_user, db_password, db_host, db_p
     # SQL command to create the users table if it doesn't exist
     create_table_command = text("""
         CREATE TABLE IF NOT EXISTS users (
-            username VARCHAR(255) PRIMARY KEY,
-            email VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            email VARCHAR(255) PRIMARY KEY,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         );
     """)
@@ -43,5 +43,22 @@ def create_database_and_users_table(db_name, db_user, db_password, db_host, db_p
         connection.execute(create_table_command)
         print("Users table created (if it didn't exist already).")
 
-# Example usage
+def delete_database(db_name, db_user, db_password, db_host, db_port):
+    # Connection string for initial connection (without database name)
+    initial_connection_string = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}"
+
+    # Create an initial engine without specifying the database
+    initial_engine = create_engine(initial_connection_string)
+
+    # SQL command to drop the database
+    # Use backticks around the database name to handle special characters
+    drop_db_command = text(f"DROP DATABASE IF EXISTS `{db_name}`;")
+
+    # Execute the SQL command to drop the database
+    with initial_engine.connect() as connection:
+        connection.execute(drop_db_command)
+        print(f"Database '{db_name}' dropped (if it existed).")
+
+
+delete_database('micro1', 'root', '', 'localhost', 3306)
 create_database_and_users_table('micro1', 'root', '', 'localhost', 3306)
