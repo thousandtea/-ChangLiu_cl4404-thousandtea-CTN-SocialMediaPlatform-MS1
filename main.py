f52a4366aa5d6a446b40a7f96b87bd1853d56274
larkJennice <zx2395@columbia.edu>
2023-12-21 21:04:37 +0000

    Final adjustment for presentation

--- main.py
+++ main.py
@@ -18,16 +18,26 @@ from jose import jwt, JWTError
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse 
 
+from fastapi.middleware.cors import CORSMiddleware
 # Database connection
-connection_string = ""
+connection_string = "mysql+pymysql://root:11223496743Yodo@localhost:3306/micro1"
 database = Database(connection_string)
 
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://35.226.190.190:4200"],  # Update with Angular app's domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Oath section
 # Google OAuth2 Config
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+CLIENT_ID = "454768390285-36tko22c48hj8ca5qhd4aha88m18cdps.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
 REDIRECT_URI = "http://localhost:8012/auth/callback"
 SCOPES = "openid email profile"
 AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth"
@@ -56,7 +66,7 @@ async def login():
     return RedirectResponse(url=url)
 
 @app.get("/auth/callback")
-async def auth_callback(code: str = None):
+async def auth_callback(code: str = Query(None)):
     if not code:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid response from Google")
 
@@ -86,25 +96,24 @@ async def auth_callback(code: str = None):
     # Create new user. (Yes, we move it here)
     user_data = Insert(username=username, email=email)
     try:
-        existing_user = users_resource.get_user(user_email)
-        return JSONResponse({
-            "message": "User already exists. Logged in successfully",
-            "access_token": token_json["access_token"],
-            "username": existing_user.username,
-            "email": existing_user.email
-        })
+        existing_user = users_resource.get_user(email)
+        message = "User already exists. Logged in successfully"
     except HTTPException as exc:
         # If user does not exist, create a new user
-        username = idinfo["given_name"] + "_" + idinfo["family_name"]
-        email = idinfo["email"]
         user_data = Insert(username=username, email=email)
         created_user = users_resource.create_user(user_data)
-        return JSONResponse({
-            "message": "New user created and logged in successfully",
-            "access_token": token_json["access_token"],
-            "username": username,
-            "email": email
-        })
+        message = "New user created and logged in successfully"
+
+    # Construct the redirect URL to your frontend with user data
+    frontend_redirect_url = "http://localhost:4200"
+    query_params = {
+        "message": message,
+        "access_token": token_json["access_token"],
+        "username": username,
+        "email": email
+    }
+    redirect_url = f"{frontend_redirect_url}?{urlencode(query_params)}"
+    return RedirectResponse(url=redirect_url)
     # try:
     #     created_user = users_resource.create_user(user_data)
     # except Exception as exc:
@@ -132,6 +141,11 @@ async def get_api_docs():
 @app.get("/api/users", response_model=List[User])
 async def get_users():
     return users_resource.get_all_users()
+
+@app.post("/api/users/", response_model=Insert)
+# Admin only
+async def create_user(user_data: Insert = Body(...), _: dict = Depends(get_current_admin)):
+    return users_resource.create_user(user_data)
 
 @app.put("/api/users/{email}", response_model=dict)
 async def update_user(email: str, new_username: str = Body(..., embed=True), _: dict = Depends(get_current_admin)):
